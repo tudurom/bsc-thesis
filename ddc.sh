fbf2d72ab80198b796e3b909b647ec4b2d933da6
Tudor Roman <tudor@tudorr.ro>
2024-06-22 21:45:03 +0200

    ddc.sh improvements

--- ddc.sh
+++ ddc.sh
@@ -11,7 +11,12 @@ usage() {
   echo "-n disables downloading."
 }
 
+log() {
+  echo "[DDC] $@"
+}
+
 cmp2() {
+  sha256sum "$1" "$2"
   local hash="$(sha256sum "$1" | awk '{print $1}')"
   (
     echo "$hash $1"
@@ -36,43 +41,43 @@ if [[ "$#" -eq 1 ]] && [[ "$1" = '-n' ]]; then
 fi
 
 if [[ "$DOWNLOAD" -ne 0 ]]; then
-  echo "Downloading Go release from $ORIGINAL_URL"
-  wget -O '/tmp/go-original.tar.gz' "$ORIGINAL_URL"
+  log "Downloading Go release from $ORIGINAL_URL"
+  wget --no-verbose -O '/tmp/go-original.tar.gz' "$ORIGINAL_URL"
 
-  echo "Downloading secondary (T) Go release from $T_URL"
-  wget -O '/tmp/go-T.tar.gz' "$T_URL"
+  log "Downloading secondary (T) Go release from $T_URL"
+  wget --no-verbose -O '/tmp/go-T.tar.gz' "$T_URL"
 else
-  echo "Not downloading..."
+  log "Not downloading..."
 fi
 
 ORIGINAL_DIR="$(mktemp -d '/tmp/ddc-release-XXXXX')"
 T_DIR="$(mktemp -d '/tmp/ddc-T-XXXXX')"
 
-echo "Untarring release to $ORIGINAL_DIR"
+log "Untarring release to $ORIGINAL_DIR"
 tar -xzf "/tmp/go-original.tar.gz" -C "$ORIGINAL_DIR"
 
-echo "Untarring T to $T_DIR"
+log "Untarring T to $T_DIR"
 tar -xzf "/tmp/go-T.tar.gz" -C "$T_DIR"
 
-echo "Creating copies"
+log "Creating copies"
 cp --reflink=auto -R "$ORIGINAL_DIR/" "${ORIGINAL_DIR}-hackseed/"
 cp --reflink=auto -R "$ORIGINAL_DIR/" "${ORIGINAL_DIR}-hack/"
 cp --reflink=auto -R "$ORIGINAL_DIR/" "${ORIGINAL_DIR}-hack-regen/"
 cp --reflink=auto -R "$ORIGINAL_DIR/" "${ORIGINAL_DIR}-compiled-with-T/"
 cp --reflink=auto -R "$ORIGINAL_DIR/" "${ORIGINAL_DIR}-compiled-with-T-regen/"
 
-echo "Removing binaries from copies"
-rm -vrf "${ORIGINAL_DIR}"-{hackseed,hack,compiled-with-T}/go/{bin/*,pkg/tool/*}
+log "Removing binaries from copies"
+rm -vrf "${ORIGINAL_DIR}"-{hackseed,hack,hack-regen,compiled-with-T,compiled-with-T-regen}/go/{bin/*,pkg/tool/*}
 
-echo "Cleaning cache"
+log "Cleaning cache"
 GOROOT="$ORIGINAL_DIR/go" PATH="$GOROOT/bin:$PATH" go clean -cache
 
-echo "Placing hack seed with evilgen"
+log "Placing hack seed with evilgen"
 GOROOT="$ORIGINAL_DIR/go" PATH="$GOROOT/bin:$PATH" \
   go run ./evilgen/main.go ./attack/syntax.go.tpl \
   > "${ORIGINAL_DIR}-hackseed/go/src/cmd/compile/internal/syntax/syntax.go"
 
-echo "Compiling seed compiler"
+log "Compiling seed compiler"
 (
   cd "${ORIGINAL_DIR}-hackseed/go/src/"
   export GOROOT_BOOTSTRAP="$ORIGINAL_DIR/go"
@@ -81,7 +86,7 @@ echo "Compiling seed compiler"
   ./make.bash
 )
 
-echo "Compiling hacked compiler using seed compiler"
+log "Compiling hacked compiler using seed compiler"
 (
   cd "${ORIGINAL_DIR}-hack/go/src/"
   export GOROOT_BOOTSTRAP="${ORIGINAL_DIR}-hackseed/go"
@@ -90,8 +95,8 @@ echo "Compiling hacked compiler using seed compiler"
   ./make.bash
 )
 
-echo "DDC PART 1: Compile A with A"
-echo "A = ${ORIGINAL_DIR}-hack"
+log "DDC PART 1: Compile A with A"
+log "A = ${ORIGINAL_DIR}-hack"
 (
   cd "${ORIGINAL_DIR}-hack-regen/go/src/"
   export GOROOT_BOOTSTRAP="${ORIGINAL_DIR}-hack/go"
@@ -100,10 +105,10 @@ echo "A = ${ORIGINAL_DIR}-hack"
   ./make.bash
 )
 
-echo "Computing hashes of A and regenerated A"
+log "Comparing hashes of A and regenerated A"
 cmp2 "${ORIGINAL_DIR}"-{hack,hack-regen}"/go/pkg/tool/linux_amd64/compile"
 
-echo "DDC PART 2: Compile A with T"
+log "DDC PART 2: Compile A with T"
 (
   cd "${ORIGINAL_DIR}-compiled-with-T/go/src/"
   export GOROOT_BOOTSTRAP="$T_DIR/go"
@@ -112,7 +117,7 @@ echo "DDC PART 2: Compile A with T"
   ./make.bash
 )
 
-echo "DDC PART 3: Compile A with A_T (A of T)"
+log "DDC PART 3: Compile A with A_T (A of T)"
 (
   cd "${ORIGINAL_DIR}-compiled-with-T-regen/go/src/"
   export GOROOT_BOOTSTRAP="${ORIGINAL_DIR}-compiled-with-T/go"
@@ -120,5 +125,5 @@ echo "DDC PART 3: Compile A with A_T (A of T)"
   ./clean.bash || true
   ./make.bash
 )
-echo "Computing hashes"
+log "Comparing hashes of A and A_A_T"
 cmp2 "${ORIGINAL_DIR}"-{compiled-with-T-regen,hack-regen}"/go/pkg/tool/linux_amd64/compile"
